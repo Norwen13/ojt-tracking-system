7160a3fbfa283adfe8a38be8662690b9608834df
Norwen Anthony Golez <student@example.com>
2026-07-17 14:25:31 +0800

    Fixed runserver

--- placement/management/commands/seed_admin.py
+++ placement/management/commands/seed_admin.py
@@ -1,24 +1,3 @@
-from django.core.management.base import BaseCommand
-from placement.models import SchoolAdmin
-
-
-class Command(BaseCommand):
-    help = "Creates (or resets) a School Admin account so you can log in for the first time."
-
-    def add_arguments(self, parser):
-        parser.add_argument("--password", default="admin123", help="Password for the admin account.")
-
-    def handle(self, *args, **options):
-        password = options["password"]
-        admin, created = SchoolAdmin.objects.get_or_create(pk=1)
-        admin.set_password(password)
-        admin.save()
-        action = "Created" if created else "Reset"
-        self.stdout.write(self.style.SUCCESS(
-            f"{action} School Admin account -> admin_id: {admin.admin_id}, password: {password}"
-        ))
-
-
 import os
 from django.contrib.auth import get_user_model
 from django.core.management import BaseCommand
